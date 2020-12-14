f47ca1e19c01dfcbc3891f167974497ca3620bf1
Rohit <rohitpratti@gmail.com>
2020-12-14 14:30:53 -0800

    updated

--- app/data/projects.js
+++ app/data/projects.js
@@ -1,17 +1,4 @@
 // TODO Add a couple lines about each project
-const data = [
-  {
-    title: 'Brohitmedia',
-    subtitle: 'Fun Firebase Social Media App',
-    link: '/projects/nearestdollar/',
-    image: '/images/projects/nearestdollar.jpg',
-    date: '2015-11-20',
-    desc:
-      'Built for a social impact hackathon. ' +
-      'NearestDollar connected to your bank accounts, credit cards, ' +
-      'or debit cards and rounded up your purchases to donate the balance to ' +
-      'the charity of your choice.',
-  },
-];
+const data = [];
 
 export default data;
--- app/views/Contact.js
+++ app/views/Contact.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -7,50 +7,36 @@ import Main from '../layouts/Main';
 
 import data from '../data/contact';
 
-// Validates the first half of an email address.
-const validateText = (text) => {
-  // NOTE: Passes RFC 5322 but not tested on google's standard.
-  // eslint-disable-next-line no-useless-escape
-  const re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))$/;
-  return re.test(text) || text.length === 0;
-};
-
-const Contact = () => {
-  return (
-    <Main>
-      <Helmet title="Contact" />
-      <article className="post" id="contact">
-        <header>
-          <div className="title">
-            <h2>
-              <Link to="/contact">Contact</Link>
-            </h2>
-          </div>
-        </header>
-        <div className="email-at">
-          <p>Feel free to get in touch. You can email me at: </p>
-          <div
-            className="inline-container"
-            onMouseEnter={() => setIsActive(false)}
-            onMouseLeave={() => idx < messages.length && setIsActive(true)}
-          >
-            <a href={`mailto:rohit.pratti@gmail.comcom`}>
-              <span>rohit.pratti@gmail.com</span>
-            </a>
-          </div>
+const Contact = () => (
+  <Main>
+    <Helmet title="Contact" />
+    <article className="post" id="contact">
+      <header>
+        <div className="title">
+          <h2>
+            <Link to="/contact">Contact</Link>
+          </h2>
         </div>
-        <ul className="icons">
-          {data.map((s) => (
-            <li key={s.label}>
-              <a href={s.link}>
-                <FontAwesomeIcon icon={s.icon} />
-              </a>
-            </li>
-          ))}
-        </ul>
-      </article>
-    </Main>
-  );
-};
+      </header>
+      <div className="email-at">
+        <p>Feel free to get in touch. You can email me at: </p>
+        <div className="inline-container">
+          <a href="mailto:rohit.pratti@gmail.comcom">
+            <span>rohit.pratti@gmail.com</span>
+          </a>
+        </div>
+      </div>
+      <ul className="icons">
+        {data.map((s) => (
+          <li key={s.label}>
+            <a href={s.link}>
+              <FontAwesomeIcon icon={s.icon} />
+            </a>
+          </li>
+        ))}
+      </ul>
+    </article>
+  </Main>
+);
 
 export default Contact;

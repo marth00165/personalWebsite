97893dc50cea4e754fc970c486e65d56987ae14e
Rohit <rohitpratti@gmail.com>
2020-12-14 17:23:22 -0800

    updated home page

--- app/views/Index.js
+++ app/views/Index.js
@@ -8,18 +8,26 @@ const Index = () => (
     <article className="post" id="index">
       <header>
         <div className="title">
-          <h2><Link to="/">About this site</Link></h2>
-          <p>A beautiful, responsive, react app written with modern Javascript.</p>
+          <h2>
+            <Link to="/">About this site</Link>
+          </h2>
+          <p>
+            A beautiful, responsive, react app written with modern Javascript.
+          </p>
         </div>
       </header>
-      <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
-        or you can check out my {' '}
-        <Link to="/resume">resume</Link>, {' '}
-        <Link to="/projects">projects</Link>, {' '}
-        view <Link to="/stats">site statistics</Link>, {' '}
-        or <Link to="/contact">contact</Link> me.
+      <p>
+        {' '}
+        Welcome to my website. Please feel free to read more{' '}
+        <Link to="/about">about me</Link>, or you can check out my{' '}
+        <Link to="/resume">resume</Link>, <Link to="/projects">projects</Link>,{' '}
+        <Link to="/contact">contact</Link> me.
       </p>
-      <p> Source available <a href="https://github.com/mldangelo/personal-site">here</a>.</p>
+      <p>
+        {' '}
+        Source available{' '}
+        <a href="https://github.com/marth00165/personalWebsite">here</a>.
+      </p>
     </article>
   </Main>
 );

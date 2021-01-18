4ce4c71db1415cc15acf2f1976fcdd54b89cc92c
Rohit <rohitpratti@gmail.com>
2021-01-17 21:28:10 -0800

    added chat w hit to projects

--- app/data/projects.js
+++ app/data/projects.js
@@ -1,3 +1,4 @@
+/* eslint-disable operator-linebreak */
 // TODO Add a couple lines about each project
 const data = [
   {
@@ -5,13 +6,24 @@ const data = [
     subtitle: 'A Simple Twitter Clone',
     link: 'https://brohitmedia2.web.app/',
     image: '/images/projects/brohitmedia.png',
-    date: '2019-12-01',
+    date: '2019-27-09',
     desc:
-      'Project Built to study Firebase'
-      + 'Built Using React/Redux/Material UI Front end'
-      + 'NodeJs Express Firebase Backend'
-      + 'Inspired by twitter'
-      + 'Allows Users to Post, Like, Create Accounts and Update description and profile pictures',
+      'Project Built to study Firebase' +
+      'Built Using React/Redux/Material UI Front end' +
+      'NodeJs Express Firebase Backend' +
+      'Inspired by twitter' +
+      'Allows Users to Post, Like, Create Accounts and Update description and profile pictures',
+  },
+  {
+    title: 'Chat With Hit',
+    subtitle: 'Chat Application made with React and Apollo GraphQL',
+    link: 'http://ec2-3-128-6-117.us-east-2.compute.amazonaws.com/login',
+    image: '/images/projects/chatwithhit.png',
+    date: '2020-17-01',
+    desc:
+      'Project Built to study Graphql and EC2 Aws instance' +
+      'Built using React/Redux/Bootstrap' +
+      'Application that allows users to create accounts and live chat',
   },
   {
     title: 'Hi-Lo',
@@ -20,8 +32,8 @@ const data = [
     image: '/images/projects/hilo.png',
     date: '2018-08-01',
     desc:
-      'Project Built as Capstone Project for Flatiron'
-      + 'Built Using Pure Javascript CSS HTML5',
+      'Project Built as Capstone Project for Flatiron' +
+      'Built Using Pure Javascript CSS HTML5',
   },
 ];
 
